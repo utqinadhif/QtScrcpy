86579de0a2c388d79430fa3d44d5a03a1c7741b9
utqinadhif <nadhifbeo@gmail.com>
2026-01-15 07:24:46 +0700

    feat: try

--- QtScrcpy/ui/dialog.cpp
+++ QtScrcpy/ui/dialog.cpp
@@ -77,7 +77,7 @@ Dialog::Dialog(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
                     ui->connectedPhoneList->addItem(Config::getInstance().getNickName(item) + "-" + item);
                 }
 
-                // add auto start for all devices by utqinadhif
+                // add auto start for all devices
                 for (int i = 0; i < ui->serialBox->count(); ++i) {
                     QString serial = ui->serialBox->itemText(i).trimmed();
                     if (!serial.isEmpty()) {
